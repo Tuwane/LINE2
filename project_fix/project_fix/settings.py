de3e517ce398e0e97154f5f72ffc755938ecc305
Tuwane <152686997+Tuwane@users.noreply.github.com>
2026-01-06 23:36:03 +0200

    deploying for rendering

--- project_fix/project_fix/settings.py
+++ project_fix/project_fix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=-=k!2ycs*+(7&h6xau$s*mjt0l%s3trl1v0bbwgi&5tzsypdd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,3 +126,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
